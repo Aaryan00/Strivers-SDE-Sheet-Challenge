298d2d5b1766589c3c4d65687255c42eae8adde5
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2023-06-11 13:39:35 +0530

    pascal triangle

---
+++ 1. Arrays/2. Pascal Triangle.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r(numRows);
+        for(int i = 0; i < numRows;i++){
+            r[i].resize(i+1);
+            r[i][i] = 1;
+            r[i][0] = 1;
+            for(int j=1;j<i;j++){
+                r[i][j] = r[i-1][j-1] + r[i-1][j];
+            }
+        }
+        return r;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> r;
+    Solution obj;
+    r = obj.generate(7);
+
+    for (const auto& row : r) {
+        for (const auto& num : row) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    return 0;
+}
