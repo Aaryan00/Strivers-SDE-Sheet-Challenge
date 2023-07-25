f0f79ef6a901f25bd2538719a13fa6fa3563c6b0
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2023-07-26 00:08:08 +0530

    M coloring

---
+++ 10. Recursion and backtracking/58. M coloring.cpp
@@ -0,0 +1,97 @@
+// using adjacent matrix as input
+//  #include <bits/stdc++.h>
+
+// bool isSafe(int node, int v, int color, vector<int> &colorOfNode, vector<vector<int>> &mat){
+//     for(int i = 0; i < v; i++){
+//         if(mat[node][i] == 1){
+//             if(colorOfNode[i] == color){
+//                 return false;
+//             }
+//         }
+//     }
+
+//     colorOfNode[node] = color;
+//     return true;
+// }
+
+// bool solve(int node, int v, int m, vector<int> &colorOfNode, vector<vector<int>> &mat){
+//     if(node == v){
+//         return true;
+//     }
+    
+//     for(int i = 1; i <= m; i++){
+//         if(isSafe(node, v, i, colorOfNode, mat)){
+//             if(solve(node+1, v, m, colorOfNode, mat)){
+//                 return true;
+//             }
+//             colorOfNode[node] = 0;
+//         }
+//     }
+
+//     return false;
+// }
+
+// string graphColoring(vector<vector<int>> &mat, int m) {
+//     int v = mat.size();
+//     vector<int> colorOfNode(v, 0);
+
+//     if(solve(0, v, m, colorOfNode, mat)){
+//         return "YES";
+//     }
+//     return "NO";
+// }
+
+// Using striver way of inityializing graph
+
+#include<bits/stdc++.h>
+
+using namespace std;
+bool isSafe(int node, int color[], bool graph[101][101], int n, int col) {
+  for (int k = 0; k < n; k++) {
+    if (k != node && graph[k][node] == 1 && color[k] == col) {
+      return false;
+    }
+  }
+  return true;
+}
+bool solve(int node, int color[], int m, int N, bool graph[101][101]) {
+  if (node == N) {
+    return true;
+  }
+
+  for (int i = 1; i <= m; i++) {
+    if (isSafe(node, color, graph, N, i)) {
+      color[node] = i;
+      if (solve(node + 1, color, m, N, graph)) return true;
+      color[node] = 0;
+    }
+
+  }
+  return false;
+}
+
+//Function to determine if graph can be coloured with at most M colours such
+//that no two adjacent vertices of graph are coloured with same colour.
+bool graphColoring(bool graph[101][101], int m, int N) {
+  int color[N] = {0};
+  if (solve(0, color, m, N, graph)) return true;
+  return false;
+}
+
+int main() {
+  int N = 4;
+  int m = 3;
+
+  bool graph[101][101];
+  memset(graph, false, sizeof graph);
+
+  // Edges are (0, 1), (1, 2), (2, 3), (3, 0), (0, 2)
+  graph[0][1] = 1; graph[1][0] = 1;
+  graph[1][2] = 1; graph[2][1] = 1;
+  graph[2][3] = 1; graph[3][2] = 1;
+  graph[3][0] = 1; graph[0][3] = 1;
+  graph[0][2] = 1; graph[2][0] = 1;
+  
+  cout << graphColoring(graph, m, N);
+
+}
