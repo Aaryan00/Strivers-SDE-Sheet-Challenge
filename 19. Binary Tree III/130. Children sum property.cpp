efb4f22a5976e79cc24d911e7066b1fd88338033
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2023-11-11 00:21:14 +0530

    Children Sum Property

---
+++ 19. Binary Tree III/130. Children sum property.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+void reorder(node * root) {
+  if (root == NULL) return;
+  int child = 0;
+  if (root -> left) {
+    child += root -> left -> data;
+  }
+  if (root -> right) {
+    child += root -> right -> data;
+  }
+
+  if (child < root -> data) {
+    if (root -> left) root -> left -> data = root -> data;
+    else if (root -> right) root -> right -> data = root -> data;
+  }
+
+  reorder(root -> left);
+  reorder(root -> right);
+
+  int tot = 0;
+  if (root -> left) tot += root -> left -> data;
+  if (root -> right) tot += root -> right -> data;
+  if (root -> left || root -> right) root -> data = tot;
+}
+void changeTree(node * root) {
+  reorder(root);
+}
+
+void inOrderTrav(node * curr, vector < int > & inOrder) {
+  if (curr == NULL)
+    return;
+
+  inOrderTrav(curr -> left, inOrder);
+  inOrder.push_back(curr -> data);
+  inOrderTrav(curr -> right, inOrder);
+}
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
+
+int main() {
+
+  struct node * root = newNode(2);
+  root -> left = newNode(35);
+  root -> left -> left = newNode(2);
+  root -> left -> right = newNode(3);
+  root -> right = newNode(10);
+  root -> right -> left = newNode(5);
+  root -> right -> right = newNode(2);
+
+  changeTree(root);
+
+  vector < int > inOrder;
+  inOrderTrav(root, inOrder);
+
+  cout << "The inOrder Traversal is : ";
+  for (int i = 0; i < inOrder.size(); i++) {
+    cout << inOrder[i] << " ";
+  }
+
+  return 0;
+}
